f1ffb07ae9751310d639980945b34efb47afe022
Bohdan <bohdanimiela@gmail.com>
2020-02-27 11:33:47 +0100

    Add header styles
     - add styles for header
     - add background image for hero section
     - build logic to change header style on scroll
     - add smooth scroll to top on logo click
     - TODO: hamburger menu for smaller viewports

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
-import initializeSlider from'./modules/slider';
+import initializeSlider from './modules/slider';
+import { isScrolled } from './modules/navbar';
+import scrollToTop from './modules/scrollToTop';
 
-window.addEventListener('load', initializeSlider);
+window.addEventListener('load', () => {
+  initializeSlider();
+  scrollToTop();
+  window.addEventListener('scroll', isScrolled);
+});
